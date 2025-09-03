e1e69db7b240ff6b367ba485a4100b6251807ba3
Heloise18 <heloisezelma18@gmail.com>
2025-09-03 11:41:37 -0300

    paths

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import{ Home } from './components/Home'
+import{ Sobre } from './components/Sobre'
+
 
 function App() {
   const [num, setNum] = useState(0)
@@ -15,6 +16,14 @@ function App() {
   return (
     <>
  
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home/>}> </Route>
+        <Route path='/sobre' element={<Sobre/>}> </Route>
+      </Routes>
+      </BrowserRouter>
+    
       <div className="card">
         <button onClick={() => setNum((num) => num + 1)}>
           clique aqui <h1>{num}</h1>
@@ -22,8 +31,9 @@ function App() {
       </div>
       
 
-      <BrowserRouter></BrowserRouter>
     </>
+
+    
   )
 }
 
