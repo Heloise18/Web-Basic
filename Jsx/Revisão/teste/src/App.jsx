252efac2531a32c9f2c8bf23199e9e1291619dde
Heloise18 <heloisezelma18@gmail.com>
2025-10-16 11:36:37 -0300

    UPDATE

--- Jsx/Revisão/teste/src/App.jsx
+++ Jsx/Revisão/teste/src/App.jsx
@@ -2,31 +2,40 @@ import { useState } from 'react'
 import './App.css'
 import { useEffect } from 'react'
 
-// Props
-function Human({nome, sobrenome, idade}){
-  return(
-  <>
-    <h1>Nome: {nome}</h1>
-    <h1>Sobrenome: {sobrenome}</h1>
-    <h1>Idade: {idade}</h1>
-  </>  
-  )
-}
+// // Props
+// function Human({nome, sobrenome, idade}){
+//   return(
+//   <>
+//     <h1>Nome: {nome}</h1>
+//     <h1>Sobrenome: {sobrenome}</h1>
+//     <h1>Idade: {idade}</h1>
+//   </>  
+//   )
+// }
 
-function Button({color, text, height, width}){
-  return(
-  <>
-    <button style={{backgroundColor: color, height:height, width: width}} >{text}</button>
-  </>  
-  )
-}
+// function Button({color, text, height, width}){
+//   return(
+//   <>
+//     <button style={{backgroundColor: color, height:height, width: width}} >{text}</button>
+//   </>  
+//   )
+// }
+
+// function ButtonTailwind
+// ({color, text, height, width}){
+//   return(
+//   <>
+//     <button className={ `${color} ${height} ${width}`} >{text}</button>
+//   </>  
+//   )
+// }
 
 
 function App() {
-  const [count, setCount] = useState(0) 
-  const [text, setText] = useState("") 
+  // const [count, setCount] = useState(0) 
+  // const [text, setText] = useState("") 
 
-  const [IsShow, setIsShow] = useState(true) 
+  // const [IsShow, setIsShow] = useState(true) 
 
   useEffect(() => {
     console.log(text)
@@ -35,27 +44,30 @@ function App() {
 
   return (
     <>
-  <button onClick={() => setCount(count + 1)}> + </button>
+  {/* <button onClick={() => setCount(count + 1)}> + </button>
   {count}
   <button onClick={() => setCount(count - 1)}> - </button>
     <br />
   <input type="text" onChange={(e) => setText(e.target.value)} />
   <h1>{text}</h1>
 
-    <br/>
- 
-    <button onClick={() => setIsShow(!IsShow)}>Disappear</button>
-    {IsShow &&
-      <div style={{backgroundColor:  'purple', width:'100px' , height:'100px'}}> </div>
+  <br/>
 
-    }
+  <button onClick={() => setIsShow(!IsShow)}>Disappear</button>
+  {IsShow &&
+    <div style={{backgroundColor:  'purple', width:'100px' , height:'100px'}}> </div>
 
-    {/* PROPS */}
-    <Human nome="HeloLinda" sobrenome="FazChinello" idade="17"/>
-    <Button color={'blue'} height={'50px'} width={'90px'}  text={'Thaís linda!'}/> 
-    <Button color={'red'} height={'50px'} width={'90px'}  text={'Thaís linda!'}/> 
-    <Button color={'green'} height={'50px'} width={'90px'}  text={'Thaís linda!'}/> 
-    
+  } */}
+{/* 
+  PROPS
+  <Human nome="HeloLinda" sobrenome="FazChinello" idade="17"/>
+  <Button color={'blue'} height={'50px'} width={'90px'}  text={'Thaís linda!'}/> 
+  <Button color={'red'} height={'50px'} width={'90px'}  text={'Thaís linda!'}/> 
+  <Button color={'green'} height={'50px'} width={'90px'}  text={'Thaís linda!'}/> 
+  <ButtonTailwind color={'bg-blue-300'} text={'sdds Beca'} height={'h-12'} width={'w-30'}  /> */}
+  
+
+
     </>
   )
 }
