8213606f58f1c3441cef65e3f8fd91ab142208a4
Heloise18 <heloisezelma18@gmail.com>
2025-10-30 11:41:49 -0300

    Op login

--- TypeScript/loloApp/app/index.tsx
+++ TypeScript/loloApp/app/index.tsx
@@ -1,34 +1,57 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text } from 'react-native';
+import { Platform, StyleSheet, View, Text, TextInput, TouchableOpacity, Button} from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
+import { useState } from 'react';
+export default function App() {
 
-export default function HomeScreen() {
+  const [text,setText] = useState("")
   return (
-    <View style ={{flex: 1, justifyContent: 'center' , alignItems: "center"}}>
-      <View style = {styles.square} >
-        <Text style = {styles.colorBlue}>
-          Hello word!!!!!
-        </Text>
+    <View style={{flex:1, backgroundColor:'#14141f'}}>
+      <View style={{ width:200, height:200,alignItems:'flex-start',  padding: 20}}> 
+        <Image  style={{width:200, height:200} }source={require('../assets/images/ordo.png')}/>
       </View>
-     </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   colorBlue: {
-    color: 'white',
-    fontSize: 12
+    color: 'black',
+    fontSize: 40,
+    fontWeight:'900'
   },
   square :{
-    height:300,
+    height:400,
     width: 300,
-    backgroundColor: 'purple'
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: "center"
+
   }
 
 })
+
+    //   <View style ={{flex: 1, justifyContent: 'center' , alignItems: "center", backgroundColor:'gray'}}>
+    //     <View>
+    //       <Text style = {styles.colorBlue}> Dante </Text> 
+    //     </View>
+
+    //   <View style ={{flex: 1, justifyContent: 'center' , alignItems: "center", backgroundColor:'gray'}}>
+    //     <View style = {styles.square} >
+          
+    //       <Image  style={{width:420, height:730} }source={require('../assets/images/dante.jpg')}/>
+    //       <Button title= "Press me" onPress={() => console.log("ola")}color={'black'}/>
+    //     </View>
+
+      
+    //     {/* <TextInput placeholder='Digite aqui ... ' onChangeText={text => setText(text) } />
+    //     <TouchableOpacity> <View><Text>botaun</Text></View> </TouchableOpacity> */}
+      
+    //   </View>
+    //  </View>
 
